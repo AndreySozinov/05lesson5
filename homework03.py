4b2b4550f5112fbad74deb9d7098c85e06bf101f
AndreySozinov <savrey@ya.ru>
2023-03-23 21:55:30 +0500

    Third task from homework solved

--- homework03.py
+++ homework03.py
@@ -1,2 +1,20 @@
 # Создайте функцию генератор чисел Фибоначчи
+NUMBER = 20
+
+
+def phoebe(number: int) -> int:
+    first = 0
+    if number >= 0:
+        yield first
+    second = 1
+    if number >= 1:
+        yield second
+    for i in range(number-1):
+        result = first + second
+        first = second
+        second = result
+        yield result
+
+
+print(*phoebe(NUMBER))
 
